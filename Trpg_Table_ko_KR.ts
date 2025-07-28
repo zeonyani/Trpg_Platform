e3d8e133476a7528556514a2a43379f8767b80b0
zeonyani <zeonyani@gmail.com>
2025-07-28 10:01:57 +0900

    250728 zeonyani 국제화

--- Trpg_Table_ko_KR.ts
+++ Trpg_Table_ko_KR.ts
@@ -2,11 +2,386 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ko_KR">
 <context>
+    <name>ChatRoomUI</name>
+    <message>
+        <location filename="ui/chatroomui.ui" line="14"/>
+        <source>Chat Room</source>
+        <translation>채팅방</translation>
+    </message>
+    <message>
+        <location filename="ui/chatroomui.ui" line="22"/>
+        <source>RoomName : </source>
+        <translation>방 이름</translation>
+    </message>
+    <message>
+        <location filename="ui/chatroomui.ui" line="52"/>
+        <source>&lt;&lt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/chatroomui.ui" line="78"/>
+        <source>text your message...</source>
+        <translation>메시지를 입력해주세요...</translation>
+    </message>
+    <message>
+        <location filename="ui/chatroomui.ui" line="85"/>
+        <source>Send</source>
+        <translation>보내기</translation>
+    </message>
+    <message>
+        <location filename="ui/chatroomui.ui" line="94"/>
+        <source>Back to the Roomlist</source>
+        <translation>목록으로 돌아가기</translation>
+    </message>
+    <message>
+        <location filename="ui/chatroomui.cpp" line="134"/>
+        <source>quit room</source>
+        <translation>방 퇴장</translation>
+    </message>
+    <message>
+        <location filename="ui/chatroomui.cpp" line="134"/>
+        <source>leave current chatroom</source>
+        <translation>현재 방에서 나갑니다</translation>
+    </message>
+    <message>
+        <location filename="ui/chatroomui.cpp" line="245"/>
+        <source>%1 (ID: %2)</source>
+        <translation>%1 (아이디: %2)</translation>
+    </message>
+    <message>
+        <location filename="ui/chatroomui.cpp" line="260"/>
+        <source>Invite Failed</source>
+        <translation>초대 실패</translation>
+    </message>
+    <message>
+        <location filename="ui/chatroomui.cpp" line="260"/>
+        <source>opponent is already in the room, please reload the userlist</source>
+        <translation>현재 이방에 있는 사용자입니다. 유저리스트를 새로고침 해주세요</translation>
+    </message>
+    <message>
+        <location filename="ui/chatroomui.cpp" line="272"/>
+        <source>Invite Requested</source>
+        <translation>초대 전송</translation>
+    </message>
+    <message>
+        <location filename="ui/chatroomui.cpp" line="272"/>
+        <source>%1 : the invitation is requested</source>
+        <translation>%1 : 초대가 전송되었습니다</translation>
+    </message>
+    <message>
+        <location filename="ui/chatroomui.cpp" line="287"/>
+        <source>Ban Failed</source>
+        <translation>강퇴 실패</translation>
+    </message>
+    <message>
+        <location filename="ui/chatroomui.cpp" line="287"/>
+        <source>You can&apos;t ban yourself</source>
+        <translation>자기 자신은 강퇴할 수 없습니다</translation>
+    </message>
+    <message>
+        <location filename="ui/chatroomui.cpp" line="302"/>
+        <source>%1</source>
+        <translation>%1</translation>
+    </message>
+    <message>
+        <location filename="ui/chatroomui.cpp" line="302"/>
+        <source>%1 : The ban could not be completed for this reason.</source>
+        <translation>%1 : 다음과 같은 이유로 강퇴가 실패했습니다.</translation>
+    </message>
+</context>
+<context>
+    <name>LobbyMainUI</name>
+    <message>
+        <location filename="ui/lobbymainui.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/lobbymainui.cpp" line="28"/>
+        <source>store</source>
+        <translation>상점</translation>
+    </message>
+    <message>
+        <location filename="ui/lobbymainui.cpp" line="29"/>
+        <source>Field Notes</source>
+        <translation>채팅방 아이템</translation>
+    </message>
+    <message>
+        <location filename="ui/lobbymainui.cpp" line="53"/>
+        <source>%1 invitation</source>
+        <translation>%1 초대</translation>
+    </message>
+    <message>
+        <location filename="ui/lobbymainui.cpp" line="54"/>
+        <source>Will you accept invitation from room %1?</source>
+        <translation>방 %1의 초대를 수락하시겠습니까?</translation>
+    </message>
+    <message>
+        <location filename="ui/lobbymainui.cpp" line="61"/>
+        <source>invitation rejected</source>
+        <translation>초대 거절</translation>
+    </message>
+    <message>
+        <location filename="ui/lobbymainui.cpp" line="61"/>
+        <source>you rejected invitation</source>
+        <translation>초대가 거절되었습니다</translation>
+    </message>
+    <message>
+        <location filename="ui/lobbymainui.cpp" line="70"/>
+        <source>ban</source>
+        <translation>강퇴</translation>
+    </message>
+    <message>
+        <location filename="ui/lobbymainui.cpp" line="70"/>
+        <source>you&apos;re banned from the room</source>
+        <translation>당신은 방에서 강퇴되었습니다</translation>
+    </message>
+</context>
+<context>
+    <name>LoginUI</name>
+    <message>
+        <location filename="ui/loginui.ui" line="14"/>
+        <source>Login</source>
+        <translation>로그인</translation>
+    </message>
+    <message>
+        <location filename="ui/loginui.ui" line="65"/>
+        <source>IP:</source>
+        <translation>서버 IP:</translation>
+    </message>
+    <message>
+        <location filename="ui/loginui.ui" line="112"/>
+        <source>Port:</source>
+        <translation>포트:</translation>
+    </message>
+    <message>
+        <location filename="ui/loginui.ui" line="140"/>
+        <source>Connect</source>
+        <translation>접속시도</translation>
+    </message>
+    <message>
+        <location filename="ui/loginui.ui" line="162"/>
+        <source>connect status:</source>
+        <translation>현재 상태:</translation>
+    </message>
+    <message>
+        <location filename="ui/loginui.ui" line="225"/>
+        <source>ID</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <location filename="ui/loginui.ui" line="255"/>
+        <source>Password</source>
+        <translation>비밀번호</translation>
+    </message>
+    <message>
+        <location filename="ui/loginui.ui" line="340"/>
+        <source>login</source>
+        <translation>로그인</translation>
+    </message>
+    <message>
+        <location filename="ui/loginui.ui" line="387"/>
+        <source>add account</source>
+        <translation>회원가입</translation>
+    </message>
+    <message>
+        <location filename="ui/loginui.cpp" line="77"/>
+        <location filename="ui/loginui.cpp" line="95"/>
+        <source>try server connect...</source>
+        <translation>서버 연결중...</translation>
+    </message>
+    <message>
+        <location filename="ui/loginui.cpp" line="102"/>
+        <source> Welcome! </source>
+        <translation> 환영합니다! </translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.ui" line="14"/>
+        <location filename="ui/mainwindow.ui" line="14"/>
         <source>MainWindow</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="41"/>
+        <source>Logined User Name : %1</source>
+        <translation>접속자 : %1</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="47"/>
+        <source>Confirm Exit</source>
+        <translation>종료</translation>
+    </message>
+    <message>
+        <location filename="ui/mainwindow.cpp" line="47"/>
+        <source>Do you really want to exit?</source>
+        <translation>Tavern Talk를 종료하시겠습니까?</translation>
+    </message>
+</context>
+<context>
+    <name>RoomDisplayUI</name>
+    <message>
+        <location filename="ui/roomdisplayui.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/roomdisplayui.ui" line="43"/>
+        <source>&lt;&lt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/roomdisplayui.cpp" line="104"/>
+        <source>User Item</source>
+        <translation>현재 소지품</translation>
+    </message>
+    <message>
+        <location filename="ui/roomdisplayui.cpp" line="113"/>
+        <source>%1 (ID: %2)</source>
+        <translation>%1 (아이디: %2)</translation>
+    </message>
+    <message>
+        <location filename="ui/roomdisplayui.cpp" line="155"/>
+        <source>current room item</source>
+        <translation>현재 방 아이템</translation>
+    </message>
+</context>
+<context>
+    <name>RoomListUI</name>
+    <message>
+        <location filename="ui/roomlistui.ui" line="20"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/roomlistui.ui" line="28"/>
+        <source>Welcome!</source>
+        <translation>환영합니다!</translation>
+    </message>
+    <message>
+        <location filename="ui/roomlistui.ui" line="38"/>
+        <source>reload</source>
+        <translation>새로고침</translation>
+    </message>
+    <message>
+        <location filename="ui/roomlistui.ui" line="47"/>
+        <source>Enter RoomName Here</source>
+        <translation>채팅방 이름을 넣어주세요</translation>
+    </message>
+    <message>
+        <location filename="ui/roomlistui.ui" line="54"/>
+        <source>Create Room</source>
+        <translation>방 만들기</translation>
+    </message>
+    <message>
+        <location filename="ui/roomlistui.ui" line="63"/>
+        <source>Join Room</source>
+        <translation>입장하기</translation>
+    </message>
+    <message>
+        <location filename="ui/roomlistui.cpp" line="122"/>
+        <source>Join Failed</source>
+        <translation>입장 실패</translation>
+    </message>
+    <message>
+        <location filename="ui/roomlistui.cpp" line="122"/>
+        <source>you&apos;ve failed to join the room</source>
+        <translation>방 입장에 실패하였습니다</translation>
+    </message>
+    <message>
+        <location filename="ui/roomlistui.cpp" line="140"/>
+        <source>Invite Failed</source>
+        <translation>초대 실패</translation>
+    </message>
+    <message>
+        <location filename="ui/roomlistui.cpp" line="140"/>
+        <source>The opponent is currently offline</source>
+        <translation>상대방이 접속하지 않았습니다</translation>
+    </message>
+</context>
+<context>
+    <name>storeUI</name>
+    <message>
+        <location filename="ui/storeui.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui/storeui.ui" line="39"/>
+        <source>Store Item</source>
+        <translation>판매중인 상품</translation>
+    </message>
+    <message>
+        <location filename="ui/storeui.ui" line="68"/>
+        <source>reload</source>
+        <translation>새로고침</translation>
+    </message>
+    <message>
+        <location filename="ui/storeui.ui" line="79"/>
+        <source>User Inventory</source>
+        <translation>현재 소지품</translation>
+    </message>
+    <message>
+        <location filename="ui/storeui.cpp" line="60"/>
+        <source>%1 (ID: %2)</source>
+        <translation>%1 (아이디: %2)</translation>
+    </message>
+    <message>
+        <location filename="ui/storeui.cpp" line="70"/>
+        <source>Error</source>
+        <translation>오류</translation>
+    </message>
+    <message>
+        <location filename="ui/storeui.cpp" line="70"/>
+        <source>The connection to the server has been lost</source>
+        <translation>서버와의 접속이 종료되었습니다</translation>
+    </message>
+    <message>
+        <location filename="ui/storeui.cpp" line="89"/>
+        <source>&apos;%1&apos; do you really want to buy this?
+Price: %2Won
+Items Left: %3</source>
+        <translation>%1 이 제품을 사시겠습니까?
+가격: %2원
+남은 수량: %3</translation>
+    </message>
+    <message>
+        <location filename="ui/storeui.cpp" line="98"/>
+        <source>Out of Stock</source>
+        <translation>재고 없음</translation>
+    </message>
+    <message>
+        <location filename="ui/storeui.cpp" line="98"/>
+        <source>This Item is currently out of stock.</source>
+        <translation>현재 아이템의 수량이 부족합니다.</translation>
+    </message>
+    <message>
+        <location filename="ui/storeui.cpp" line="102"/>
+        <source>Have one</source>
+        <translation>이미 있는 아이템</translation>
+    </message>
+    <message>
+        <location filename="ui/storeui.cpp" line="102"/>
+        <source>you already have this Item</source>
+        <translation>이미 아이템을 가지고 있습니다</translation>
+    </message>
+    <message>
+        <location filename="ui/storeui.cpp" line="119"/>
+        <source>Purchase Successful</source>
+        <translation>구매 성공</translation>
+    </message>
+    <message>
+        <location filename="ui/storeui.cpp" line="120"/>
+        <source>%1 You have successfully purchased the item.
+Items left: %2</source>
+        <translation>%1 아이템을 성공적으로 구매했습니다.
+남은 수량 : %2</translation>
+    </message>
+    <message>
+        <location filename="ui/storeui.cpp" line="149"/>
+        <source>Price: %1won&lt;br&gt;Item left: %2</source>
+        <translation>가격: %1원&lt;br&gt;남은 수량: %2</translation>
     </message>
 </context>
 </TS>
